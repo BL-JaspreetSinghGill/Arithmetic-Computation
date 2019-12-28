6e64a57a0198c4b4c28a26a03f811267639a287f
JaspreetSinghGill <jaspreetsg06@gmail.com>
2019-12-28 11:25:02 +0530

    ADDED: Performed arithmetic calculation mentioned in use case 4

---
+++ ucFourArithmeticCalculation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+#ARITHMETIC COMPUTATION AND SORTING : USE CASE 5 -> Calculating c + a / b;
+
+takeUserInput () {
+	read -p "ENTER THE FIRST NUMBER : " firstNumber;
+	read -p "ENTER THE SECOND NUMBER : " secondNumber;
+	read -p "ENTER THE THIRD NUMBER : " thirdNumber;
+}
+
+arithmeticCalculation1 () {
+	result1=$(($firstNumber+$secondNumber*$thirdNumber));
+
+	echo $result1;
+}
+
+arithmeticCalculation2 () {
+   result2=$(($firstNumber*$secondNumber+$thirdNumber));
+
+   echo $result2;
+}
+
+arithmeticCalculation3 () {
+   result3=$(($thirdNumber+$firstNumber/$secondNumber));
+
+   echo $result3;
+}
+
+arithmeticMain () {
+	takeUserInput;
+	arithmeticCalculation1;
+	arithmeticCalculation2;
+	arithmeticCalculation3;
+}
+
+arithmeticMain;
